dea85dc04be7aadcbb8a1a5b769287d561f53ee0
mateus zebendo <mateuszebendovp30@gmail.com>
2024-01-18 00:18:57 -0300

    terminei o dia 1

---
+++ day-1-printing-start/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao Gerador de Nomes de Bandas!")
+city = input("Digite o nome da sua cidade natal: \n")
+pet = input("Digite o nome do seu animal de estimação: \n")
+print("O nome da sua banda poderia ser " + city + " " + pet) 
